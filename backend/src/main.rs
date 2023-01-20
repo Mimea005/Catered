606e2f1db14e4c9c08afbe743a19380c2e7ffbf3
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2023-01-20 16:08:37 +0100

    Added boilerplate server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,24 @@
+use std::env;
+
+use actix_web::{HttpServer, App, web};
+
 
 #[actix_web::main]
-async fn main() {
-    async {println!("Hello, world!")}.await;
+async fn main() -> std::io::Result<()>{
+    let port = env::var("PORT")
+        .unwrap_or(String::from("8080"))
+        .parse()
+        .expect("Failed to conert port to number");
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+    })
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
+}
+
+async fn hello() -> &'static str {
+    "Hello!"
 }
