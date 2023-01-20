40d1ce905a4af4617cb069bae504b6fd4efe8be0
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2023-01-20 23:20:58 +0100

    Added some middleware

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
-use actix_web::{HttpServer, App, web};
+use actix_session::{SessionMiddleware, storage::CookieSessionStore};
+use actix_web::{HttpServer, App, web, middleware::{Logger, NormalizePath, TrailingSlash,}, cookie::Key};
 
 pub mod scopes;
 use scopes::api;
@@ -10,10 +11,20 @@ async fn main() -> std::io::Result<()>{
     let port = env::var("PORT")
         .unwrap_or(String::from("8080"))
         .parse()
-        .expect("Failed to conert port to number");
+    .expect("Failed to conert port to number");
+
+    let log_env = env_logger::Env::new().filter("LOGLEVEL");
+
+    env_logger::init_from_env(log_env);
 
     HttpServer::new(|| {
         App::new()
+            .wrap(
+                SessionMiddleware::builder(CookieSessionStore::default(), Key::from(&[1; 64]))
+                .build()
+            )
+            .wrap(NormalizePath::new(TrailingSlash::Always))
+            .wrap(Logger::default())
             .service(web::scope("/api").configure(api::configure))
     })
     .bind(("0.0.0.0", port))?
