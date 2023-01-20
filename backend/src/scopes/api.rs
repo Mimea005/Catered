c0f04f2e433fa440f33f429155b017292624f4b7
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2023-01-20 23:21:24 +0100

    Added mock api

--- backend/src/scopes/api.rs
+++ backend/src/scopes/api.rs
@@ -1,13 +1,74 @@
-use actix_web::web;
+use actix_web::{web, get, Responder};
 
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg
-        .route("/", web::get().to(api_info))
+        .service(api_info)
+        .service(
+            web::scope("/dishes")
+                .configure(dishes::configure)
+        )
     ;
 }
 
-
+#[get("/")]
 async fn api_info() -> &'static str {
-    "This is Catered's first api (V1)\n:>"
+"
+This is Catered's first api (V1)
+    
+:>
+"
+}
+
+mod dishes {
+    use super::*;
+    use actix_web::{Either, HttpResponse};
+    use serde_json::json;
+    use types::Dish;
+    
+    const MENU: [Dish; 2] = [
+        types::Dish {
+            name: "Bro ko",
+            description: "A Brothers koli",
+            price: (55, 99),
+            publisher: "Kao"
+        },
+        types::Dish {
+            name: "Brolognese",
+            description: "Would you share with a bro?",
+            price: (322, 49),
+            publisher: "Kao"
+        }
+    ];
+    
+    pub fn configure(cfg: &mut web::ServiceConfig) {
+        cfg
+            .service(get_menu)
+            .service(get_dish)
+        ;
+    }
+
+    #[get("/")]
+    async fn get_menu() -> impl Responder {
+        let dishes = MENU.clone()
+            .map(|d| {
+                json!({
+                    "name": d.name,
+                    "description": d.description
+                })
+            });
+        
+        web::Json(dishes)
+    }
+
+    #[get("/{name}/")]
+    async fn get_dish(req: web::Path<String>) -> Either<web::Json<Dish<'static>>, HttpResponse> {
+        let found = MENU.iter()
+            .filter(|d| d.name == req.clone())
+            .next();
+        match found {
+            Some(dish) => Either::Left(web::Json(dish.clone())),
+            None => Either::Right(HttpResponse::NotFound().body("Not a dish").into())
+        }
+    }
 }
--- types/src/lib.rs
+++ types/src/lib.rs
@@ -8,3 +8,19 @@ pub struct Dish<'a> {
     pub price: (isize, usize),
     pub publisher: &'a str
 }
+
+pub struct User<'a> {
+    pub name: &'a str,
+    pub role: Role
+}
+
+/// The role of a user describes the relation between the
+/// service and the user e.g
+/// Customer: is a customer, can only by and contact
+/// Regular: same as a customer with extra benefits
+/// Admin: administrator, can add, edit, remove and communicate with costumers
+pub enum Role {
+    Customer,
+    Regular(isize),
+    Admin
+}
